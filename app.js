ffa5120cb3ac5a294cba7b7da0d1dce11d16feb6
Olexandra Korol <korol.olexandra@gmail.com>
2024-06-23 19:10:33 +0300

    show winners

--- app.js
+++ app.js
@@ -8,25 +8,74 @@ Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
+
+    }
+  },
+  watch:{
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = 'Draw'
+      } else if (value <= 0) {
+        this.winner = 'monster'
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = 'Draw'
+      } else if (value <= 0) {
+        this.winner = 'player'
+      }
     }
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%'}
+      } 
       return {width: this.monsterHealth + '%'}
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%'}
+      }
       return {width: this.playerHealth + '%'}
+    },
+    mayUseSpacialAttack() {
+      return this.currentRound % 3 !== 0
     }
   },
   methods: {
     attackMonster() {
-      const attacValue = attack(5, 12)
+      this.currentRound++
+      const attacValue = attack(4, 10)
       this.monsterHealth -= attacValue
       this.attackPlayer()
     },
     attackPlayer() {
       const attacValue = attack(8, 15)
       this.playerHealth -= attacValue
-    }
+    },
+    specialAttacMonster() {
+      const attackValue = attack(10, 25)
+      this.monsterHealth -= attackValue
+      this.attackPlayer()
+    },
+    healPlayer() {
+      const healValue = attack(8, 20) 
+      if ( this.playerHealth + healValue > 100 ) {
+        this.playerHealth = 100
+      } else {
+        this.playerHealth += healValue
+      }
+      this.attackPlayer()
+    },
+    startGame(){
+      this.playerHealth = 100,
+      this.monsterHealth = 100,
+      this.currentRound = 0,
+      this.winner = null
+    },
   }
 }).mount('#game')
